bb0fc46602d9966e5b1532d02cc66b00a21ac3d0
Georgios Moutsos <g.moutsos@neurocom.gr>
2025-06-07 14:39:05 +0300

    feat: generate json file from source code with categories and list of faces

--- generate-faces-html.sh
+++ generate-faces-html.sh
@@ -9,3 +9,14 @@ for category in clock complication demo io sensor settings; do
         echo "<option data-category=\"$category\" data-url=\"https://github.com/joeycastillo/second-movement/blob/$commit/$f\" value=\"$(basename $f .h)\">$(basename $f _face.h)</option>"
     done
 done
+
+#generate json file
+echo -n "[" > faces.json
+for category in clock complication demo io sensor settings; do
+    echo -n "{\"category\": \"$category\", \"faces\": [" >> faces.json
+    for f in "watch-faces/$category"/*.h; do
+        echo -n "\"$(basename $f .h)\"," >> faces.json
+    done
+    echo -n "]}," >> faces.json
+done
+echo -n "]" >> faces.json
