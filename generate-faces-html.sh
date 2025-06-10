97f00cc6ddd9d162a6386014d14907ea27630c04
Georgios Moutsos <g.moutsos@neurocom.gr>
2025-06-10 11:53:47 +0300

    fix: change faces.json output path

--- generate-faces-html.sh
+++ generate-faces-html.sh
@@ -11,16 +11,16 @@ for category in clock complication demo io sensor settings; do
 done
 
 #generate json file
-echo -n "[" > faces.json
+echo -n "[" > /static/faces.json
 for category in clock complication demo io sensor settings; do
-    echo -n "{\"category\": \"$category\", \"faces\": [" >> faces.json
+    echo -n "{\"category\": \"$category\", \"faces\": [" >> /static/faces.json
     for f in "watch-faces/$category"/*.h; do
-        echo -n "{\"face\": \"$(basename $f _face.h)\"," >> faces.json
+        echo -n "{\"face\": \"$(basename $f _face.h)\"," >> /static/faces.json
         doc=`sed -n '/#define\|#pragma/,${p;/#include/q}' < $f | sed '1d;$d;s/"/\\"/;s/\/*//;s/^[ ]*\*[ ]*$//;s/^[ ]*\* //;s/[ ]*\*\/[ ]*$//;/^$/d;'`
         doc=${doc//$'\n'/\\n}
         doc=${doc//\"/\\\"}
-        echo -n "\"doc\": \"${doc}\"}," >> faces.json
+        echo -n "\"doc\": \"${doc}\"}," >> /static/faces.json
     done
-    echo -n "]}," >> faces.json
+    echo -n "]}," >> /static/faces.json
 done
-echo -n "]" >> faces.json
+echo -n "]" >> /static/faces.json
