6bf036f2ff3f22a33a265802feb561f12cbdcc32
Georgios Moutsos <g.moutsos@neurocom.gr>
2025-06-10 11:32:22 +0300

    fix: refine the faces documentation extraction

--- generate-faces-html.sh
+++ generate-faces-html.sh
@@ -16,7 +16,7 @@ for category in clock complication demo io sensor settings; do
     echo -n "{\"category\": \"$category\", \"faces\": [" >> faces.json
     for f in "watch-faces/$category"/*.h; do
         echo -n "{\"face\": \"$(basename $f _face.h)\"," >> faces.json
-        doc=`sed -n '/define\|pragma/,${p;/include/q}' < $f | sed '1d;$d;s/"/\\"/;s/\/*//;s/^[ ]*\*[ ]*$//;s/^[ ]*\* //;s/[ ]*\*\/[ ]*$//;/^$/d;'`
+        doc=`sed -n '/#define\|#pragma/,${p;/#include/q}' < $f | sed '1d;$d;s/"/\\"/;s/\/*//;s/^[ ]*\*[ ]*$//;s/^[ ]*\* //;s/[ ]*\*\/[ ]*$//;/^$/d;'`
         doc=${doc//$'\n'/\\n}
         doc=${doc//\"/\\\"}
         echo -n "\"doc\": \"${doc}\"}," >> faces.json
